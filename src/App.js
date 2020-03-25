00be398492997fcff93ab0e508393d26dc04d3ab
Nathen Bowes <nathen.bowes@shopify.com>
2020-03-25 15:15:54 -0700

    add useState hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState }from 'react';
 
 function App() {
+  const[userQuery, setUserQuery] = useState();
+
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery)
+    setUserQuery(event.target.value)
+  }
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  }
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  }
+
   return (
     <div className="App">
-      App
+      <h1>Hello Nathen</h1>
+      <div className="form">
+        <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress} />
+        <button onClick={searchQuery}>Search</button>
+      </div>
     </div>
   );
 }
